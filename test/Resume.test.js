850002987d138aea544eb84019dd215ea71f34a4
Joseph Sungpil Choi <chousemath@gmail.com>
2018-06-23 13:17:49 +0900

    Add some unit tests

--- test/Resume.test.js
+++ test/Resume.test.js
@@ -3,11 +3,19 @@ const ganache = require('ganache-cli');
 // this is a constructor function
 const Web3 = require('web3');
 const mocha = require('mocha');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let resume;
+
+// const constructorOwner = 'Joseph Sungpil Choi';
+// const constructorIntroduction = 'I am just a small time boy, livin in a lonely world';
+// const constructorCurrentJob = 'Software Engineer';
+//       1990,
+//       7,
+//       7
 
 mocha.beforeEach(async () => {
   // get a list of all accounts
@@ -27,25 +35,27 @@ mocha.beforeEach(async () => {
     from: accounts[0],
     gas: 1e6.toString()
   });
+  resume.setProvider(provider);
 });
 
 mocha.describe('Resume', () => {
   mocha.it('deploys a contract', () => {
-    // use one of the accounts to deploy the contract
-    console.log(resume);
+    assert.ok(resume.options.address);
+  });
+  mocha.it('initializes the contract with the values passed to the constructor', async () => {
+    const owner = await resume.methods.owner().call();
+    const introduction = await resume.methods.introduction().call();
+    const currentJob = await resume.methods.currentJob().call();
+    const birthYear = await resume.methods.birthYear().call();
+    const birthMonth = await resume.methods.birthMonth().call();
+    const birthDay = await resume.methods.birthDay().call();
+
+    assert.equal(owner, 'Joseph Sungpil Choi');
+  });
+  mocha.it('successfully updates a person\'s introduction', () => {
+
+  });
+  mocha.it('successfully updates a person\'s current job', () => {
+
   });
 });
-
-
-// let car;
-// mocha.beforeEach(() => {
-//   car = new Car();
-// });
-// mocha.describe('Car', () => {
-//   mocha.it('should return stopped when parked', () => {
-//     assert.equal(car.park(), 'stopped');
-//   });
-//   mocha.it('should return vroom when being drived', () => {
-//     assert.equal(car.drive(), 'vroom');
-//   });
-// });
