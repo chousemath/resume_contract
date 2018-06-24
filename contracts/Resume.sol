79e332f1fcd9c277cb7b752b2aeda49679768abe
Joseph Sungpil Choi <chousemath@gmail.com>
2018-06-24 13:59:05 +0900

    All gender assignment and gender modification

--- contracts/Resume.sol
+++ contracts/Resume.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.17;
 
 contract Resume {
-    string public owner;
-    string public introduction;
+    // solidity enums start at 0
+    enum Genders { Unspecified, Male, Female, Other }
+    string public uuid; // could be social security number, once set, cannot be changed
+    string public ownerName; // government-issued name
+    string public introduction; // quick introduction of who this person is
     string public currentJob;
     string public currentAddress;
     string public currentPhone;
@@ -10,9 +13,11 @@ contract Resume {
     uint public birthYear; // can be unsigned integer
     uint public birthMonth;
     uint public birthDay;
+    Genders public gender;
 
     constructor(
-        string _owner,
+        string _uuid,
+        string _ownerName,
         string _introduction,
         string _currentJob,
         string _currentAddress,
@@ -20,9 +25,11 @@ contract Resume {
         string _currentEmail,
         uint _birthYear,
         uint _birthMonth,
-        uint _birthDay
+        uint _birthDay,
+        Genders _gender
     ) public {
-        owner = _owner;
+        uuid = _uuid;
+        ownerName = _ownerName;
         introduction = _introduction;
         currentJob = _currentJob;
         currentAddress = _currentAddress;
@@ -31,17 +38,36 @@ contract Resume {
         birthYear = _birthYear;
         birthMonth = _birthMonth;
         birthDay = _birthDay;
+
+        int genderInt = int(_gender);
+        gender = (genderInt < 0 || genderInt > 3) ? Genders.Unspecified : _gender;
     }
 
     function setCurrentAddress(string newAddress) public {
         currentAddress = newAddress;
     }
 
+    function setCurrentEmail(string newEmail) public {
+        currentEmail = newEmail;
+    }
+
     function setCurrentJob(string newJob) public {
         currentJob = newJob;
     }
 
+    function setCurrentPhone(string newPhone) public {
+        currentPhone = newPhone;
+    }
+
+    function setGender(Genders newGender) public {
+        gender = newGender;
+    }
+
     function setIntroduction(string newIntroduction) public {
         introduction = newIntroduction;
+    }
+
+    function setOwnerName(string newOwnerName) public {
+        ownerName = newOwnerName;
     }
 }
--- test/Resume.test.js
+++ test/Resume.test.js
@@ -6,11 +6,15 @@ const mocha = require('mocha');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
+// require('events').EventEmitter.defaultMaxListeners = 50;
 
 let accounts;
 let resume;
+let resume2;
+let resume3;
 
-const _owner = 'Joseph Sungpil Choi';
+const _uuid = '0123456789';
+const _ownerName = 'Joseph Sungpil Choi';
 const _introduction = 'I am just a small time boy, livin in a lonely world';
 const _currentJob = 'Software Engineer';
 const _currentAddress = '2761 Millers Way Drive, Ellicott City, Maryland, 21043';
@@ -19,6 +23,9 @@ const _currentPhone = '01068513003';
 const _birthYear = 1990;
 const _birthMonth = 7;
 const _birthDay = 7;
+const _gender = 1;
+const _gender2 = 2;
+const _gender3 = 3;
 
 mocha.beforeEach(async () => {
   // get a list of all accounts
@@ -27,7 +34,8 @@ mocha.beforeEach(async () => {
   resume = await new web3.eth.Contract(JSON.parse(interface)).deploy({
     data: bytecode,
     arguments: [
-      _owner,
+      _uuid,
+      _ownerName,
       _introduction,
       _currentJob,
       _currentAddress,
@@ -35,11 +43,50 @@ mocha.beforeEach(async () => {
       _currentEmail,
       _birthYear,
       _birthMonth,
-      _birthDay
+      _birthDay,
+      _gender
     ]
   }).send({
     from: accounts[0],
-    gas: 1e6.toString()
+    gas: '4000000'
+  });
+  resume2 = await new web3.eth.Contract(JSON.parse(interface)).deploy({
+    data: bytecode,
+    arguments: [
+      _uuid,
+      _ownerName,
+      _introduction,
+      _currentJob,
+      _currentAddress,
+      _currentPhone,
+      _currentEmail,
+      _birthYear,
+      _birthMonth,
+      _birthDay,
+      _gender2
+    ]
+  }).send({
+    from: accounts[1],
+    gas: '4000000'
+  });
+  resume3 = await new web3.eth.Contract(JSON.parse(interface)).deploy({
+    data: bytecode,
+    arguments: [
+      _uuid,
+      _ownerName,
+      _introduction,
+      _currentJob,
+      _currentAddress,
+      _currentPhone,
+      _currentEmail,
+      _birthYear,
+      _birthMonth,
+      _birthDay,
+      _gender3
+    ]
+  }).send({
+    from: accounts[2],
+    gas: '4000000'
   });
   resume.setProvider(provider);
 });
@@ -49,7 +96,8 @@ mocha.describe('Resume', () => {
     assert.ok(resume.options.address);
   });
   mocha.it('initializes the contract with the values passed to the constructor', async () => {
-    const owner = await resume.methods.owner().call();
+    const uuid = await resume.methods.uuid().call();
+    const ownerName = await resume.methods.ownerName().call();
     const introduction = await resume.methods.introduction().call();
     const currentJob = await resume.methods.currentJob().call();
     const currentAddress = await resume.methods.currentAddress().call();
@@ -58,8 +106,12 @@ mocha.describe('Resume', () => {
     const birthYear = await resume.methods.birthYear().call();
     const birthMonth = await resume.methods.birthMonth().call();
     const birthDay = await resume.methods.birthDay().call();
+    const gender = await resume.methods.gender().call();
+    const gender2 = await resume2.methods.gender().call();
+    const gender3 = await resume3.methods.gender().call();
 
-    assert.equal(owner, _owner);
+    assert.equal(uuid, _uuid);
+    assert.equal(ownerName, _ownerName);
     assert.equal(introduction, _introduction);
     assert.equal(currentJob, _currentJob);
     assert.equal(currentAddress, _currentAddress);
@@ -68,6 +120,9 @@ mocha.describe('Resume', () => {
     assert.equal(birthYear, _birthYear);
     assert.equal(birthMonth, _birthMonth);
     assert.equal(birthDay, _birthDay);
+    assert.equal(gender, _gender);
+    assert.equal(gender2, _gender2);
+    assert.equal(gender3, _gender3);
   });
 
   mocha.it('successfully updates a person\'s current address', async () => {
@@ -75,6 +130,13 @@ mocha.describe('Resume', () => {
     await resume.methods.setCurrentAddress(newAddress).send({ from: accounts[0] });
     const currentAddress = await resume.methods.currentAddress().call();
     assert.equal(currentAddress, newAddress);
+  });
+
+  mocha.it('successfully updates a person\'s current job', async () => {
+    const newEmail = 'testcase@beepb00p.club';
+    await resume.methods.setCurrentEmail(newEmail).send({ from: accounts[0] });
+    const currentEmail = await resume.methods.currentEmail().call();
+    assert.equal(currentEmail, newEmail);
   });
   
   mocha.it('successfully updates a person\'s current job', async () => {
@@ -84,10 +146,31 @@ mocha.describe('Resume', () => {
     assert.equal(currentJob, newJob);
   });
 
+  mocha.it('successfully updates a person\'s current phone number', async () => {
+    const newPhone = '021233003';
+    await resume.methods.setCurrentPhone(newPhone).send({ from: accounts[0] });
+    const currentPhone = await resume.methods.currentPhone().call();
+    assert.equal(currentPhone, newPhone);
+  });
+
+  mocha.it('successfully updates a person\'s current gender', async () => {
+    const newGender = 3;
+    await resume.methods.setGender(newGender).send({ from: accounts[0] });
+    const currentGender = await resume.methods.gender().call();
+    assert.equal(currentGender, newGender);
+  });
+
   mocha.it('successfully updates a person\'s introduction', async () => {
     const newIntroduction = 'i am a new introduction';
     await resume.methods.setIntroduction(newIntroduction).send({ from: accounts[0] });
     const introduction = await resume.methods.introduction().call();
     assert.equal(introduction, newIntroduction);
+  });
+
+  mocha.it('successfully updates a person\'s name', async () => {
+    const newOwnerName = 'Steven Yuen';
+    await resume.methods.setOwnerName(newOwnerName).send({ from: accounts[0] });
+    const ownerName = await resume.methods.ownerName().call();
+    assert.equal(ownerName, newOwnerName);
   });
 });
