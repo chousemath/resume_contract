7c29a490abfe4b8d61a30f17f0faf77bbb6d2842
Joseph Sungpil Choi <chousemath@gmail.com>
2018-06-29 12:25:50 +0900

    Add some notes, update deploy script

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 * Smart contracts are “immutable”. Once they are deployed, their code is impossible to change, making it impossible to fix any discovered bugs.
 * Strings are considered dynamic arrays in Solidity, because we are unable to retrieve nested arrays through web3, that means we are unable to fetch arrays of strings through web3
 * This contract uses IPFS for all document and image storage
+* Any time we deploy a contract, our contract is assigned a specific address on the blockchain
 
 ### What is a `confirmation date`?
 
--- deploy.js
+++ deploy.js
@@ -5,27 +5,17 @@ const config = require('./config.json');
 const provider = new HDWalletProvider(config.accountMnemonic, config.nodeLink);
 const web3 = new Web3(provider);
 
+console.log(interface);
+
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+  // const accounts = await web3.eth.getAccounts();
   const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({
     data: '0x' + bytecode,
-    arguments: [
-      '0123456789',
-      'John Speagul Chan',
-      'I am an aspiring blockchain engineer',
-      'Software Engineer',
-      '373 Gangnam-daero Seocho-gu Seoul',
-      '0201231234',
-      'gloopglop@beepb00p.club',
-      1989,
-      4,
-      3,
-      1
-    ]
+    arguments: [] // My constructor needs no arguments
   }).send({
     gas: 5e6.toString(),
-    from: accounts[0]
+    from: config.mainAccount
   });
-  console.log('\n\n\n>>> ', result.options.address);
+  console.log('\n\nADDRESS:\n ', result.options.address);
 };
 deploy().then(() => console.log('\nfinished deploying contract!')).catch(err => console.log(err));
